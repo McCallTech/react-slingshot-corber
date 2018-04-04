bf541316aa94c692061739ce202d10682a74247f
Josh McCall <joshmccall221@gmail.com>
2018-04-04 03:29:13 +0000

    updates

--- go.sh
+++ go.sh
@@ -1,31 +1,46 @@
-#sudo add-apt-repository ppa:webupd8team/java
-#sudo apt-get update
-#sudo apt-get install java-common oracle-java8-installer
-#sudo apt-get install oracle-java8-set-default
-#source /etc/profile
-#
-#export JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
-#export PATH=$JAVA_HOME/bin:$PATH
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install java-common oracle-java8-installer
+sudo apt-get install oracle-java8-set-default
+source /etc/profile
 
-#sudo add-apt-repository ppa:cwchien/gradle
-#sudo apt-get update
-#sudo apt-get install gradle
-#sudo add-apt-repository ppa:maarten-fonville/android-studio
-#sudo apt-get update
-#sudo apt-get install android-studio
+export JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
+export PATH=$JAVA_HOME/bin:$PATH
+
+sudo add-apt-repository ppa:cwchien/gradle
+sudo apt-get update
+sudo apt-get install gradle
+sudo add-apt-repository ppa:maarten-fonville/android-studio
+sudo apt-get update
+sudo apt-get install android-studio
 
 
-export ANDROID_HOME="/home/crou/Android/Sdk/"
-export PATH="${ANDROID_HOME}tools/:${ANDROID_HOME}platform-tools/:${PATH}";
-#~/Android/Sdk/tools/bin/sdkmanager "platform-tools" "platforms;android-19" "build-tools;19.1.0" "system-images;android-19;google_apis;x86"
 
-. ~/.nvm/nvm.sh; nvm use --lts;
+# download android sdk
+wget https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip
+sudo apt-get install unzip
+mkdir -p ~/Android/Sdk/
+unzip sdk-tools-linux-3859397.zip -d ~/Android/Sdk/
+
+export ANDROID_HOME="/home/$(whoami)/Android/Sdk/"
+export PATH="${ANDROID_HOME}tools/bin/:${ANDROID_HOME}tools/:${ANDROID_HOME}platform-tools/:${PATH}";
+
+# install all sdk packages
+##android update sdk --no-ui
+sdkmanager --update
+sdkmanager "platform-tools" "build-tools;26.0.3"  "platforms;android-26"
+ls ${ANDROID_HOME}
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+. ~/.nvm/nvm.sh; nvm i --lts;
 echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+#yarn install
+npm i -g yarn ember-cli corber cordova http-server
+rm -rf corber
+corber init
 #corber platform remove android
 #corber platform add android
-#yarn install
-#corber init
-#corber proxy requirements
+corber proxy requirements
 #corber serve --platform=android
 corber build --platform=android
 sed -i 's%href="/%href="%g' ./corber/cordova/www/index.html
